8eb0082a16243ba3b42af56400de0b7846fb6f9d
remshadm@gmail.com <remshadm@gmail.com>
2021-01-10 21:54:45 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import ssl
+
 from website.app import create_app
 
 
@@ -6,4 +8,18 @@ app = create_app({
     'OAUTH2_REFRESH_TOKEN_GENERATOR': True,
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
+    'OAUTHLIB_INSECURE_TRANSPORT':1
+
 })
+
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+#app.run()
+#
+# context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+# context.verify_mode = ssl.CERT_REQUIRED
+# context.load_verify_locations("localhost.crt")
+# context.load_cert_chain("localhost.crt", "localhost.key")
+
+app.run(ssl_context="adhoc")
